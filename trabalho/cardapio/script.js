38af4d220d43253cfd69db228a6d97cdc9ffd918
AsuraTest <155957483+AsuraTest@users.noreply.github.com>
2024-03-11 10:26:14 -0300

    Polo Pinho
    Trabalho com html, css e script.

---
+++ trabalho/cardapio/script.js
@@ -0,0 +1,10 @@
+
+    var btn1 = document.getElementById('btn1').addEventListener('click', () =>{    window.open("formulario.html"); });
+
+    var btn2 = document.getElementById('btn2').addEventListener('click', () =>{    window.open("formulario.html"); });
+
+    var btn3 = document.getElementById('btn3').addEventListener('click', () =>{    window.open("formulario.html"); });
+
+    var btn4 = document.getElementById('btn4').addEventListener('click', () =>{    window.open("formulario.html"); });
+
+    var btn5 = document.getElementById('btn5').addEventListener('click', () =>{    window.open("pedido.html"); });
